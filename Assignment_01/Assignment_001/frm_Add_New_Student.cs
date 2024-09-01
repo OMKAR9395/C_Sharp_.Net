ec6883eafcd71509c75320ed0d7f62e07d6f2680
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-01 17:00:43 +0530

    Assignment_01

---
+++ Assignment_01/Assignment_001/frm_Add_New_Student.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_001
+{
+    public partial class frm_Add_New_Student : Form
+    {
+        public frm_Add_New_Student()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=Patil;Initial Catalog=Assignment_1System_DB;Integrated Security=True");
+        void Con_Open()
+        {
+            if (Con.State != ConnectionState.Open)
+            {
+                Con.Open();
+            }
+        }
+
+        void Con_Close()
+        {
+            if (Con.State != ConnectionState.Closed)
+            {
+                Con.Close();
+            }
+        }
+        private void Only_Numeric(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Only_Text(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsLetter(e.KeyChar) || (e.KeyChar == (char)Keys.Back) || (e.KeyChar == (char)Keys.Space)))
+            {
+                e.Handled = true;
+            }
+        }
+        private void frm_Add_New_Student_Load(object sender, EventArgs e)
+        {
+
+        }
+        void Clear_Controls()
+        {
+            tb_Roll_No.Clear();
+            tb_Name.Clear();
+            tb_Mob_No.Clear();
+
+            dtp_DOB.Text = "1/1/2010";
+            cb_Course.SelectedIndex = -1;
+
+            tb_Roll_No.Focus();
+        }
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+
+            if (tb_Roll_No.Text != "" && tb_Name.Text != "" && tb_Mob_No.Text != "" && cb_Course.Text != "")
+            {
+                SqlCommand Cmd = new SqlCommand();
+
+                Cmd.Connection = Con;
+                Cmd.CommandText = "Insert Into Student_Details (Roll_No, Name, Mobile_No, DOB, Course) values (@RNo, @Nm, @MobNo, @DOB, @Course )";
+
+                Cmd.Parameters.Add("RNo", SqlDbType.Int).Value = tb_Roll_No.Text;
+                Cmd.Parameters.Add("Nm", SqlDbType.VarChar).Value = tb_Name.Text;
+                Cmd.Parameters.Add("MobNo", SqlDbType.Decimal).Value = tb_Mob_No.Text;
+                Cmd.Parameters.Add("DOB", SqlDbType.Date).Value = dtp_DOB.Text;
+                Cmd.Parameters.Add("Course", SqlDbType.NVarChar).Value = cb_Course.Text;
+
+                Cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student Info Saved", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                Clear_Controls();
+            }
+            else
+            {
+                MessageBox.Show("INCOMPLETE INFO", "Fill All Fields", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            Con_Close();
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+        }
+
+        private void btn_Student_List_Click(object sender, EventArgs e)
+        {
+            frm_Student_List obj = new frm_Student_List();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            frm_Login Obj = new frm_Login();
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Assignment_01/Assignment_001/frm_Student_List.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Assignment_001
+{
+    partial class frm_Student_List
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_Header = new System.Windows.Forms.Label();
+            this.dgv_Student_List = new System.Windows.Forms.DataGridView();
+            this.rollNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mobileNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dobDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.assignment1SystemDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.assignment_1System_DBDataSet = new Assignment_001.Assignment_1System_DBDataSet();
+            this.btn_Add_New_student = new System.Windows.Forms.Button();
+            this.btn_Log_Out = new System.Windows.Forms.Button();
+            this.student_DetailsTableAdapter = new Assignment_001.Assignment_1System_DBDataSetTableAdapters.Student_DetailsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Student_List)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentDetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assignment1SystemDBDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assignment_1System_DBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Header
+            // 
+            this.lbl_Header.AutoSize = true;
+            this.lbl_Header.BackColor = System.Drawing.Color.GhostWhite;
+            this.lbl_Header.Font = new System.Drawing.Font("Modern No. 20", 55F, System.Drawing.FontStyle.Bold);
+            this.lbl_Header.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_Header.Location = new System.Drawing.Point(264, 9);
+            this.lbl_Header.Name = "lbl_Header";
+            this.lbl_Header.Size = new System.Drawing.Size(512, 94);
+            this.lbl_Header.TabIndex = 0;
+            this.lbl_Header.Text = "Student List";
+            // 
+            // dgv_Student_List
+            // 
+            this.dgv_Student_List.AllowUserToAddRows = false;
+            this.dgv_Student_List.AllowUserToDeleteRows = false;
+            this.dgv_Student_List.AutoGenerateColumns = false;
+            this.dgv_Student_List.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgv_Student_List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Student_List.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rollNoDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.mobileNoDataGridViewTextBoxColumn,
+            this.dobDataGridViewTextBoxColumn,
+            this.courseDataGridViewTextBoxColumn});
+            this.dgv_Student_List.DataSource = this.studentDetailsBindingSource;
+            this.dgv_Student_List.Location = new System.Drawing.Point(2, 109);
+            this.dgv_Student_List.Name = "dgv_Student_List";
+            this.dgv_Student_List.ReadOnly = true;
+            this.dgv_Student_List.RowHeadersWidth = 51;
+            this.dgv_Student_List.RowTemplate.Height = 24;
+            this.dgv_Student_List.Size = new System.Drawing.Size(1084, 471);
+            this.dgv_Student_List.TabIndex = 1;
+            this.dgv_Student_List.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Student_List_CellContentClick);
+            // 
+            // rollNoDataGridViewTextBoxColumn
+            // 
+            this.rollNoDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.rollNoDataGridViewTextBoxColumn.DataPropertyName = "Roll_No";
+            this.rollNoDataGridViewTextBoxColumn.HeaderText = "Roll No";
+            this.rollNoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.rollNoDataGridViewTextBoxColumn.Name = "rollNoDataGridViewTextBoxColumn";
+            this.rollNoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // mobileNoDataGridViewTextBoxColumn
+            // 
+            this.mobileNoDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.mobileNoDataGridViewTextBoxColumn.DataPropertyName = "Mobile_No";
+            this.mobileNoDataGridViewTextBoxColumn.HeaderText = "Mobile No";
+            this.mobileNoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.mobileNoDataGridViewTextBoxColumn.Name = "mobileNoDataGridViewTextBoxColumn";
+            this.mobileNoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // dobDataGridViewTextBoxColumn
+            // 
+            this.dobDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dobDataGridViewTextBoxColumn.DataPropertyName = "Dob";
+            this.dobDataGridViewTextBoxColumn.HeaderText = "Dob";
+            this.dobDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.dobDataGridViewTextBoxColumn.Name = "dobDataGridViewTextBoxColumn";
+            this.dobDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // courseDataGridViewTextBoxColumn
+            // 
+            this.courseDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.courseDataGridViewTextBoxColumn.DataPropertyName = "Course";
+            this.courseDataGridViewTextBoxColumn.HeaderText = "Course";
+            this.courseDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.courseDataGridViewTextBoxColumn.Name = "courseDataGridViewTextBoxColumn";
+            this.courseDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // studentDetailsBindingSource
+            // 
+            this.studentDetailsBindingSource.DataMember = "Student_Details";
+            this.studentDetailsBindingSource.DataSource = this.assignment1SystemDBDataSetBindingSource;
+            // 
+            // assignment1SystemDBDataSetBindingSource
+            // 
+            this.assignment1SystemDBDataSetBindingSource.DataSource = this.assignment_1System_DBDataSet;
+            this.assignment1SystemDBDataSetBindingSource.Position = 0;
+            // 
+            // assignment_1System_DBDataSet
+            // 
+            this.assignment_1System_DBDataSet.DataSetName = "Assignment_1System_DBDataSet";
+            this.assignment_1System_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btn_Add_New_student
+            // 
+            this.btn_Add_New_student.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_Add_New_student.Font = new System.Drawing.Font("Modern No. 20", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add_New_student.ForeColor = System.Drawing.Color.DarkCyan;
+            this.btn_Add_New_student.Location = new System.Drawing.Point(335, 586);
+            this.btn_Add_New_student.Name = "btn_Add_New_student";
+            this.btn_Add_New_student.Size = new System.Drawing.Size(394, 55);
+            this.btn_Add_New_student.TabIndex = 2;
+            this.btn_Add_New_student.Text = "Add New Student";
+            this.btn_Add_New_student.UseVisualStyleBackColor = false;
+            this.btn_Add_New_student.Click += new System.EventHandler(this.btn_Add_New_student_Click);
+            // 
+            // btn_Log_Out
+            // 
+            this.btn_Log_Out.BackColor = System.Drawing.Color.Cornsilk;
+            this.btn_Log_Out.Font = new System.Drawing.Font("MS Outlook", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Log_Out.ForeColor = System.Drawing.Color.Red;
+            this.btn_Log_Out.Location = new System.Drawing.Point(949, 9);
+            this.btn_Log_Out.Name = "btn_Log_Out";
+            this.btn_Log_Out.Size = new System.Drawing.Size(121, 42);
+            this.btn_Log_Out.TabIndex = 3;
+            this.btn_Log_Out.Text = "Log Out";
+            this.btn_Log_Out.UseVisualStyleBackColor = false;
+            this.btn_Log_Out.Click += new System.EventHandler(this.btn_Log_Out_Click);
+            // 
+            // student_DetailsTableAdapter
+            // 
+            this.student_DetailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // frm_Student_List
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.btn_Log_Out);
+            this.Controls.Add(this.btn_Add_New_student);
+            this.Controls.Add(this.dgv_Student_List);
+            this.Controls.Add(this.lbl_Header);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Student_List";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student List";
+            this.Load += new System.EventHandler(this.frm_Student_List_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Student_List)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentDetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assignment1SystemDBDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assignment_1System_DBDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Header;
+        private System.Windows.Forms.DataGridView dgv_Student_List;
+        private System.Windows.Forms.Button btn_Add_New_student;
+        private System.Windows.Forms.Button btn_Log_Out;
+        private System.Windows.Forms.BindingSource assignment1SystemDBDataSetBindingSource;
+        private Assignment_1System_DBDataSet assignment_1System_DBDataSet;
+        private System.Windows.Forms.BindingSource studentDetailsBindingSource;
+        private Assignment_1System_DBDataSetTableAdapters.Student_DetailsTableAdapter student_DetailsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rollNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mobileNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dobDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseDataGridViewTextBoxColumn;
+    }
+}
